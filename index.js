8063e8a6dcac7b5fd21fee8002a8691448597397
theMorshed <themorshedctg@gmail.com>
2022-12-28 00:07:36 +0600

    add task, delete task, get all completed task api written

--- index.js
+++ index.js
@@ -1,6 +1,73 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+
+// taskManagerUser  rif2Rnnomgsejq6R
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = "mongodb+srv://taskManagerUser:rif2Rnnomgsejq6R@cluster0.zkjorm4.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const taskCollection = client.db('taskManager').collection('tasks');
+
+        // get all active task 
+        app.get('/my-task', async (req, res) => {
+            const query = {
+                status: 'active'
+            };
+            const result = await taskCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        // get all task which is already completed
+        app.get('/completetask', async (req, res) => {
+            const query = {
+                status: 'complete'
+            };
+            const result = await taskCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        // add a task in database
+        app.post('/addtask', async (req, res) => {
+            const task = req.body;
+            const result = await taskCollection.insertOne(task);
+            res.send(result);
+        });
+
+        // update task
+        app.put('/updatetask/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true }
+            const updatedDoc = {
+                $set: {
+                    status: 'complete'
+                }
+            }
+            const result = await taskCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        });
+
+        // delete task by admin from mongodb
+        app.delete('/deletetask/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const result = await taskCollection.deleteOne(filter);
+            res.send(result);
+        });
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err));
+
 
 app.get("/", (req, res) => {
     res.send("Hello from task manager server.");
